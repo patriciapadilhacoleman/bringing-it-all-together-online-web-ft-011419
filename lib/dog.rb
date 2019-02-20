9d5258e704ead92aecff4ee83fd5520202d84dbf
PatPadilha Coleman <patricia.p.coleman@gmail.com>
2019-02-20 12:15:52 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,63 @@
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(id:nil, name:, breed:)
+
+    @id = id
+    @name = name
+    @breed = breed
+
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+
+  end
+
+  def self.new_from_db(row)
+    new_dog = self.new  # self.new is the same as running Song.new
+    new_dog.id = row[0]
+    new_dog.name =  row[1]
+    new_dog.breed = row[2]
+    new_dog  # return the newly created instance
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+end
